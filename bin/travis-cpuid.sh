a807e5f90cd7d379c25ba5c741d732eb14142cf5
Marcin Bielak <marcin.bieli@gmail.com>
2013-10-18 00:24:25 +0200

    upgrade source/docs/travis.yml for compiling with PHP 5.4 + PHP 5.5

--- bin/travis-cpuid.sh
+++ bin/travis-cpuid.sh
@@ -16,26 +16,27 @@ cd cpuid-php-extension
 phpize
 CFLAGS="-m32" ./configure
 make
+make test
 
 sudo make install
 #echo "extension=cpuid_extension.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
-#PHP_LIB_PATH=`grep -r cpuid_extension.so  ~/.phpenv/versions/$(phpenv version-name)/lib/php/extensions/ | grep -oP "\/.[^\.]*.so"`
-PHP_LIB_PATH=~/.phpenv/versions/$(phpenv version-name)/lib/php/extensions/no-debug-zts-20100525/cpuid_extension.so
+###PHP_LIB_PATH=`grep -r cpuid_extension.so  ~/.phpenv/versions/$(phpenv version-name)/lib/php/extensions/ | grep -oP "\/.[^\.]*.so"`
+#PHP_LIB_PATH=~/.phpenv/versions/$(phpenv version-name)/lib/php/extensions/no-debug-zts-20100525/cpuid_extension.so
 
-echo "+==================================================================+"
-echo "PHP_LIB_PATH: '$PHP_LIB_PATH'"
-ls $PHP_LIB_PATH -la
-echo "+==================================================================+"
+#echo "+==================================================================+"
+#echo "PHP_LIB_PATH: '$PHP_LIB_PATH'"
+#ls $PHP_LIB_PATH -la
+#echo "+==================================================================+"
 
-sudo mkdir -p /lib/php/extensions/no-debug-zts-20100525
-sudo chmod a+wrx /lib/php/extensions/no-debug-zts-20100525
-sudo cp $PHP_LIB_PATH /lib/php/extensions/no-debug-zts-20100525/cpuid_extension.so
-sudo chmod a+wrx /lib/php/extensions/no-debug-zts-20100525/cpuid_extension.so
+#sudo mkdir -p /lib/php/extensions/no-debug-zts-20100525
+#sudo chmod a+wrx /lib/php/extensions/no-debug-zts-20100525
+#sudo cp $PHP_LIB_PATH /lib/php/extensions/no-debug-zts-20100525/cpuid_extension.so
+#sudo chmod a+wrx /lib/php/extensions/no-debug-zts-20100525/cpuid_extension.so
 
-echo "+==================================================================+"
-ls /lib/php/extensions/no-debug-zts-20100525/cpuid_extension.so -la
-echo "+==================================================================+"
+#echo "+==================================================================+"
+#ls /lib/php/extensions/no-debug-zts-20100525/cpuid_extension.so -la
+#echo "+==================================================================+"
 
 
-echo "extension=$PHP_LIB_PATH" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+#echo "extension=$PHP_LIB_PATH" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
