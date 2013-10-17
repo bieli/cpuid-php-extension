8c3d7de83bd32e7d41813e5d40bf9396032109c0
Marcin Bielak <marcin.bieli@gmail.com>
2013-10-18 00:08:26 +0200

    upgrade source/docs/travis.yml for compiling with PHP 5.4 + PHP 5.5

--- bin/travis-cpuid.sh
+++ bin/travis-cpuid.sh
@@ -20,7 +20,14 @@ make
 sudo make install
 #echo "extension=cpuid_extension.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
-PHP_LIB_PATH=`grep -r cpuid_extension.so  ~/.phpenv/versions/$(phpenv version-name)/lib/php/extensions/ | grep -oP "\/.[^\.]*.so"`
-#PHP_LIB_PATH=~/.phpenv/versions/$(phpenv version-name)/lib/php/extensions/no-debug-zts-20100525/cpuid_extension.so
+#PHP_LIB_PATH=`grep -r cpuid_extension.so  ~/.phpenv/versions/$(phpenv version-name)/lib/php/extensions/ | grep -oP "\/.[^\.]*.so"`
+PHP_LIB_PATH=~/.phpenv/versions/$(phpenv version-name)/lib/php/extensions/no-debug-zts-20100525/cpuid_extension.so
+
+echo "+==================================================================+"
+echo "PHP_LIB_PATH: '$PHP_LIB_PATH'"
+ls $PHP_LIB_PATH -la
+echo "+==================================================================+"
+
+sudo cp $PHP_LIB_PATH /lib/php/extensions/no-debug-zts-20100525/cpuid_extension.so
 
 echo "extension=$PHP_LIB_PATH" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
